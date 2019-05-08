0f80f0a4189a8556334ff3884c61785c2dac4e70
ChaoZhang <nengchibawanfan@163.com>
2019-05-08 17:14:52 +0800

    加入做市深度指标

--- src/api/marketMakeAssessment.js
+++ src/api/marketMakeAssessment.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 
 function getMarketMakeAssessment(params) {
 // body...
-  // return axios.get('http://18.179.204.45:5001/market_make_assessment', { params: params })
-  return axios.get('http://127.0.0.1:5001/market_make_assessment', { params: params })
+  return axios.get('http://18.179.204.45:5001/market_make_assessment', { params: params })
+  // return axios.get('http://127.0.0.1:5001/market_make_assessment', { params: params })
 }
 
 function getMarketMakeAssessmentInfo(params) {
 // body...
-  // return axios.get('http://18.179.204.45:5001/market_make_assessment_details', { params: params })
-  return axios.get('http://127.0.0.1:5001/market_make_assessment_details', { params: params })
+  return axios.get('http://18.179.204.45:5001/market_make_assessment_details', { params: params })
+  // return axios.get('http://127.0.0.1:5001/market_make_assessment_details', { params: params })
 }
 
 module.exports = {
